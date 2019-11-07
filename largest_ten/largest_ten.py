d3a84a3021dfcf343876dbf27e4cd9c4c9aba54d
xujinghua <jinghua.xu@student.uni-tuebingen.de>
2019-11-07 23:05:03 +0100

    ex01

--- largest_ten/largest_ten.py
+++ largest_ten/largest_ten.py
@@ -1,11 +1,20 @@
+import timeit
 
 """ Data Structures and Algorithms for CL III, WS 2019-2020, Assignment 1
    
     Problem 1: Largest ten.
 
-    <Please insert your data and the honor code here.>
+   Course:      Data Structures and Algorithms for CL III - WS1920
+   Assignment:  lab 1, exercise 1
+   Author:      Jinghua Xu
+   Description: Implement an efficient algorithm for finding the ten largest elements in a sequence of size n. 
+   Using the big-Oh notation, characterize the number of arithmetic operations of the algorithm you have written, and write analysis in the function docstring.
+ 
+ Honor Code:  I pledge that this program represents my own work.
 """
-
+ #are we expected to implemt the sorting algorithm?
+ #Question: Are we expected to treat data set of different size differently? If so, how do we define large data set? Or do we only consider the size of data sets in test?(100) 
+ #add comments in every single line of codes?
 
 def find_largest(seq, k=10):
     """ Finds the largest k elements of a sequence.
@@ -21,9 +30,22 @@ def find_largest(seq, k=10):
     -------
     largest_k : list
         List containing the largest k elements of the sequence.
-    """        
+    """     
+
+    start = timeit.timeit()
+
     largest_k = []
 
-    # FIXME your code goes here
+    for i in range(0, k):
+        maxi = max(seq)
+        seq.remove(maxi)
+        largest_k.append(maxi)
+
+    end = timeit.timeit()
+    print(end - start) 
+
+    ##an even more efficient implementation:
+
+
 
     return largest_k
