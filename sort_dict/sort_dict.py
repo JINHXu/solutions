de27ba61f746142a55feef610eb511e3f3fd1eed
xujinghua <jinghua.xu@student.uni-tuebingen.de>
2019-11-22 12:23:22 +0100

    ex05 does not work

--- sort_dict/sort_dict.py
+++ sort_dict/sort_dict.py
@@ -30,8 +30,51 @@ def string_sort(word_list):
 
     sorted_word_list = []
 
-    # building a trie from the list of strings
+    #determine d(the length of the longest string in word_list)
+    d = 0
+    for word in word_list:
+        if len(word) > d:
+            d = len(word)
+    #pre-processing on the strings: add placeholders '.' to make them all share the same length d
+    new_word_list = []
+    for word in word_list:
+        diff = d - len(word)
+        if diff > 0:
+            #appending processed word(word + right number of placeholders) to new_word_list(list of processed words)
+            new_word_list.append(word.lower() + diff * '.')
+    #creating alphabet: currently considered the full alphabet of English chars
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    #dictionary stores all the key-value pairs of alphabet for comparison
+    dict_ab = {}
+    for idx, letter in enumerate(alphabet):
+        dict_ab.update({letter: idx})
+    dict_ab.update({'.': 26})
 
-    # traversing it pre-order
+    #for d down to 1
+    for idx in list(range(d))[::-1]:
+
+        #update new_word_list for next iteration by sorting
+        new_word_list = bucket_sort(new_word_list, idx, dict_ab)
+
+    sorted_word_list = new_word_list
 
     return sorted_word_list
+
+def bucket_sort(word_list, idx, dict_ab):
+
+    sorted_list = []
+    #bucket capacity: key range(default full English alphabet plus placehold '.')
+    bucket = []
+    for _ in range(27):
+        bucket.append([])
+    #phase 1: sequence to bucket
+    for word in word_list:
+        bucket_number = dict_ab.get(word[idx])
+        bucket[bucket_number].append(word)
+
+    #phase 2: bucket to return list
+    for i in range(27):
+        for j in range(len(bucket[i])):
+            sorted_list.append(bucket[i][j])
+
+    return sorted_list
